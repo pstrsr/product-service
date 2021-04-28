72dc8fb58884ebc8f1d7e5e8e6c6d528b3d2e0e1
Peter Straßer <peter.str@hotmail.de>
2021-04-28 18:52:38 +0200

    created folder structure for backend with modules and made compilable

---
+++ backend/product-config/src/test/java/com/github/almostfamiliar/config/ConfigApplicationTests.java
@@ -0,0 +1,11 @@
+package com.github.almostfamiliar.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
